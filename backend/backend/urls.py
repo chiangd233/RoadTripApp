d7acfd491cb6d2a60e036bb4309f7ad219f9560d
chiangd233 <chiangd233@gmail.com>
2022-05-07 15:32:11 -0400

    urls.py complete

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from roadtrip import views
+
+router = routers.DefaultRouter()
+router.register(r'roadtrip', views.RoadTripView, 'roadtrip')
+router.register(r'thingstodo', views.ThingsTodoView, 'thingstodo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(routers.urls))
 ]
