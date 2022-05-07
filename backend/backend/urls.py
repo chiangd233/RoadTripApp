2b8b067477f5ebadfe27f4f469861aa74bc6d191
chiangd233 <chiangd233@gmail.com>
2022-05-07 16:37:54 -0400

    added jwt auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from roadtrip import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'roadtrip', views.RoadTripView, 'roadtrip')
@@ -24,5 +25,7 @@ router.register(r'thingstodo', views.ThingsTodoView, 'thingstodo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
 ]
