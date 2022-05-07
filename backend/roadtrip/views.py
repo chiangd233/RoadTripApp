5e55e645dc510d3a0a3b4ba14fa45999493addb2
chiangd233 <chiangd233@gmail.com>
2022-05-07 15:29:23 -0400

    created views

--- backend/roadtrip/views.py
+++ backend/roadtrip/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import RoadTripSerializer, ThingsTodoSerializer
+from .models import RoadTrip, ThingsTodo
 # Create your views here.
+
+class RoadTripView(viewsets.ModelViewSet):
+    serializer_class = RoadTripSerializer
+    queryset = RoadTrip.objects.all()
+    
+class ThingsTodoView(viewsets.ModelViewSet):
+    serializer_class = ThingsTodoSerializer
+    queryset = ThingsTodo.objects.all()
